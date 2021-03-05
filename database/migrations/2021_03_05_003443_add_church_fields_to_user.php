ffcd92a7a67490d001288e7f5aed97a18176f8bc
Darcy <darcy.bronson@gmail.com>
2021-03-04 19:52:55 -0500

    Updated Models/Migrations

---
+++ database/migrations/2021_03_05_003443_add_church_fields_to_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddChurchFieldsToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('user_type')->default(0);
+            $table->boolean('active')->default(true);
+            $table->unsignedBigInteger('church_id')->default(0);
+            //$table->foreign('church')->references('id')->on('churches');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
