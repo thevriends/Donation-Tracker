e082b8ab89e46393210e9f3b70033f1733d404b8
lyon0114 <lyon0114@algonquinlive.com>
2021-03-08 18:52:59 -0500

    Added a login blade form.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+</head>
+<body>
+<div class="box-border h-300 w-200 p-4 border-4 bg-brown">
+    <h1 class="font-custom text-5xl pb-5">Donation Tracker</h1>
+    <div class="mb-6">
+      <label for="username">
+        Username
+      </label>
+      <input class="border" id="username:" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label for="password">
+        Password
+      </label>
+      <input id="password" type="password" placeholder="******************">
+      <p class="text-red text-xs italic">Please choose a password.</p>
+    </div>
+    <div>
+      <button type="button">
+        Sign In
+      </button>
+      <href="#">
+        Forgot Password?
+      </a>
+    </div>
+</div>
+</body>
+</html>
