5002927cdaeba4322a7b763937e073df2d7f162f
Darcy <darcy.bronson@gmail.com>
2021-03-16 19:43:47 -0400

    Fixed Seeder

--- database/seeders/ChurchSeeder.php
+++ database/seeders/ChurchSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Church;
 
 class ChurchSeeder extends Seeder
 {
@@ -13,24 +14,18 @@ class ChurchSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('churches')->insert([
-        //    'name' => Str::'Test church',
-         //   'address' => Str::'123 Fake street, Ottawa, Ontario, canada, H0H0H0',
-        //    'creator' => Str::'TestAdmin',
-        //    'active' => Bool:: True,
-
-        App\church::create([
+        Church::create([
             'name' => 'Test church',
             'address' => '123 Fake street, Ottawa, Ontario, Canada, H0H0H0',
             'creator' => 'TestAdmin',
             'active' => True
-        ])
-        App\church::create([
+        ]);
+    
+        Church::create([
             'name' => 'Test church 2',
             'address' => '456 Fake street, Ottawa, Ontario, Canada, H0H0H0',
             'creator' => 'TestAdmin2',
             'active' => True
-        ])
         ]);
     }
 }
