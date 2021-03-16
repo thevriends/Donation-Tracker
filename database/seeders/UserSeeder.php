abf8afd380bcc9009859b34ea3c18aa3752243bd
Darcy <darcy.bronson@gmail.com>
2021-03-16 19:38:57 -0400

    Fixed Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -13,23 +15,23 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        App\users::create([
+        User::create([
             'name' => 'TestAdmin',
             'email' => 'TestTracker1@tracker.com',
             'password' => Hash::make('password'),
             'user_type' => 'SuperAdmin',
             'active' => True,
             'church_id' => '0'
-        ])
+        ]);
 
-        App\users::create([
+        User::create([
             'name' => 'TestAdmin2',
             'email' => 'TestTracker2@tracker.com',
             'password' => Hash::make('password'),
             'user_type' => 'Admin',
             'active' => True,
             'church_id' => '1'
-        ])
+        ]);
 
 
     }
